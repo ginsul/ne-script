6ac483116b5cb871437958c97f6cf1209d551b73
ginsul <79474779+ginsul@users.noreply.github.com>
2024-07-16 02:06:02 +0700

    berhasil KALO BELUM ADA DI NETBOX, DITAMBAHIN

--- function2-netboxcheck.py
+++ function2-netboxcheck.py
@@ -4,10 +4,16 @@
 
 import subprocess
 import re
+import pynetbox
 
 # Define arrays
 hosts_up = []
 hosts_down = []
+
+# List of CIDR blocks to scan
+cidr_blocks = ["172.25.30.202/31"]
+
+nb = pynetbox.api('http://172.25.30.129:8000', token='347076470e3d60698be8d50d00d1be814ff7dd97')
 
 # Function to resolve hostnames to IP addresses
 def resolve_ip(hostname):
@@ -15,9 +21,6 @@ def resolve_ip(hostname):
     if result.stdout:
         return result.stdout.split()[0]
     return None
-
-# List of CIDR blocks to scan
-cidr_blocks = ["172.25.30.200/28", "192.168.1.0/24"]
 
 # Run nmap for each CIDR block
 for cidr in cidr_blocks:
@@ -31,13 +34,17 @@ for cidr in cidr_blocks:
             if "[host down]" in line:
                 current_ip = line.split()[-3]  # Correct index for down hosts
                 hosts_down.append(f'"{current_ip}"')
+                print(current_ip)
 
                 try:
                     ip_address = nb.ipam.ip_addresses.get(address=str(current_ip)).status
 
                 except (AttributeError):
-                    print("IP: " + str(current_ip) + " Has not Status on Netbox")
+                    ip_address = 0
 
+
+
+            #############################################################################
             else:
                 current_ip = line.split()[-1]  # Correct index for up hosts
 
@@ -46,8 +53,16 @@ for cidr in cidr_blocks:
                     resolved_ip = resolve_ip(current_ip)
                     if resolved_ip:
                         current_ip = resolved_ip
-                
+
                 hosts_up.append(f'"{current_ip}"')
+                print(current_ip)
+
+                try:
+                    ip_address = nb.ipam.ip_addresses.get(address=str(current_ip)).status
+
+                #KALO BELUM ADA DI NETBOX, DITAMBAHIN
+                except (AttributeError):
+                    ip_address = nb.ipam.ip_addresses.create(address=str(current_ip))
 
 # Print arrays in the desired format
 print(f'Hosts up: [{", ".join(hosts_up)}]')
